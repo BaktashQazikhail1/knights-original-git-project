164d6110eaaa31a273a50ad30cae1df56c4a3016
BaktashQazikhail1 <146789326+BaktashQazikhail1@users.noreply.github.com>
2023-10-09 20:48:23 -0700

    Update MyFirstClass.java

--- src/tek/java/MyFirstClass.java
+++ src/tek/java/MyFirstClass.java
@@ -4,5 +4,6 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
         System.out.println("This is new Changes");
+        System.out.println("Added by Baktash Qazikhail");
     }
 }
