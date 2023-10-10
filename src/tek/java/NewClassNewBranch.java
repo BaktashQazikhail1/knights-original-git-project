43cac55bd195c0d89d63a040582594dae75270b6
Baktash Qazikhail <baktash_qz@yahoo.com>
2023-10-09 22:58:51 -0700

    This is just for Practic

--- src/tek/java/NewClassNewBranch.java
+++ src/tek/java/NewClassNewBranch.java
@@ -3,5 +3,6 @@ package tek.java;
 public class NewClassNewBranch {
     public static void main(String[] args) {
         System.out.println("This the class from new branch");
+        System.out.println("This Change is for Practice");
     }
 }
