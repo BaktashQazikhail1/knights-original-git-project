3f21d8b5b9c479ec61008f000614937d7ba7af92
Baktash Qazikhail <baktash_qz@yahoo.com>
2023-10-09 21:07:50 -0700

    Added By Ahmad

--- src/tek/java/MyFirstClass.java
+++ src/tek/java/MyFirstClass.java
@@ -5,5 +5,6 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("This is new Changes");
         System.out.println("Added by Baktash Qazikhail");
+        System.out.println("Ahmad");
     }
 }
